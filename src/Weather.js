11312dfe8ab12f2d220212923b7be5118a6c0381
ana-adriana <103989277+ana-adriana@users.noreply.github.com>
2022-11-13 13:59:15 +0000

    Added axios to display weather

---
+++ src/Weather.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function WeatherApp() {
+  const [city, setCity] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Search for city"
+        onChange={updateCity}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature:{weather.temperature}</li>
+          <li>Wind: {weather.wind}</li>
+          <li>Description:{weather.description}</li>
+          <li>
+            <img src={weather.icon} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
+
